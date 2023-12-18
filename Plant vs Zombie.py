e1cde822849ef256aeca8d403947b87ed7def616
EwenCheung <142422759+EwenCheung@users.noreply.github.com>
2023-12-19 00:39:53 +0800

    log_in_method

---
+++ Plant vs Zombie.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+def sign_up(username, password):
+    with open('/Users/Ewen/Documents/GitHub/PSP0101-TL9L-02/user.txt', mode='a') as f:
+        f.write(f'{username}----{password}\n')
+
+
+def check_username_taken(input_username):
+    with open('/Users/Ewen/Documents/GitHub/PSP0101-TL9L-02/user.txt', mode='rt', encoding='utf-8') as f:
+        for line in f:
+            username, password = line.strip().split('----')
+            if input_username == username:
+                print('Username Taken, Please choose an another one')
+                break
+        else:
+            return 'y'
+
+
+def check_user_pass(input_username, input_password):
+    with open('/Users/Ewen/Documents/GitHub/PSP0101-TL9L-02/user.txt', mode='rt', encoding='utf-8') as f:
+        for line in f:
+            username, password = line.strip().split('----')
+            if input_username == username and input_password == password:
+                return 'y'
+
+        else:
+            print('No account found. Check your username and password. You have to register an account to sign in')
+
+
+log_in_method = input('Sign up an account enter "U" \n'
+                      'Sign in an account enter "I"\n'
+                      'Log in as guest enter "G"\n\n'
+                      'Please enter your choice here :  ').strip().upper()
+
+asking_log_in_method = True
+
+while asking_log_in_method:
+    if log_in_method == 'G':  # log in as guest
+        print('Logged in as guest')
+        asking_log_in_method = False
+
+    elif log_in_method == 'U':  # sign up an account
+        while True:
+            input_username = input('Enter your username: ').strip()
+            res = check_username_taken(input_username)
+            if res == 'y':
+                input_password = input('Enter your password: ').strip()
+                print('Successfully registered')
+                break
+
+        sign_up(input_username, input_password)
+        asking_log_in_method = False
+
+    elif log_in_method == 'I':
+        while True:
+            input_username = input('Enter your username: ').strip()
+            input_password = input('Enter your password: ').strip()
+            res = check_user_pass(input_username, input_password)
+            if res == 'y':
+                print('Logged in')
+                break
+
+        asking_log_in_method = False
+
+    else:
+        print('Please key in the correct symbol')
