bb7494e6a3b9ed850d744cce517bad1ebc6dfb6f
EwenCheung <142422759+EwenCheung@users.noreply.github.com>
2023-12-25 12:46:37 +0800

    improve the performance of code

--- Plant vs Zombie.py
+++ Plant vs Zombie.py
@@ -88,38 +88,52 @@ while asking_log_in_method:
 # game
 import pygame
 from sys import exit
-import time
 from random import randint, choice
+
+pygame.init()  # starting code
+
+screen = pygame.display.set_mode((1000, 600))  # screen size
+pygame.display.set_caption('Pokemon vs Naruto')  # title name
+clock = pygame.time.Clock()
+game_active = True
+bg_music = pygame.mixer.Sound('audio/Plants vs. Zombies (Main Theme).mp3')
+bg_music.play(loops=-1)
+
+# load images
+naruto_frames = [pygame.image.load('Picture/naruto/naruto_walk_1.png').convert_alpha(),
+                 pygame.image.load('Picture/naruto/naruto_walk_2.png').convert_alpha(),
+                 pygame.image.load('Picture/naruto/naruto_walk_3.png').convert_alpha()]
+
+sasuke_frame = [pygame.image.load('Picture/sasuke/sasuke_walk_1.png').convert_alpha(),
+                pygame.image.load('Picture/sasuke/sasuke_walk_2.png').convert_alpha(),
+                pygame.image.load('Picture/sasuke/sasuke_walk_3.png').convert_alpha()]
+
+kakashi_frame = [pygame.image.load('Picture/kakashi/kakashi_run_1.png').convert_alpha(),
+                 pygame.image.load('Picture/kakashi/kakashi_run_2.png').convert_alpha(),
+                 pygame.image.load('Picture/kakashi/kakashi_run_3.png').convert_alpha()]
+
+naruto_frames = [pygame.transform.scale(frame, (84, 40)) for frame in naruto_frames]
+sasuke_frame = [pygame.transform.scale(frame, (84, 40)) for frame in sasuke_frame]
+kakashi_frame = [pygame.transform.scale(frame, (84, 40)) for frame in kakashi_frame]
 
 
 class Zombie(pygame.sprite.Sprite):
     def __init__(self, type, position_list_y):
         super().__init__()
 
-        if type == 'naruto':
-            naruto_1 = pygame.image.load('Picture/naruto/naruto_walk_1.png').convert_alpha()
-            naruto_2 = pygame.image.load('Picture/naruto/naruto_walk_2.png').convert_alpha()
-            naruto_3 = pygame.image.load('Picture/naruto/naruto_walk_3.png').convert_alpha()
-            self.frames = [naruto_1, naruto_2, naruto_3]
-        elif type == 'sasuke':
-            sasuke_1 = pygame.image.load('Picture/sasuke/sasuke_walk_1.png').convert_alpha()
-            sasuke_2 = pygame.image.load('Picture/sasuke/sasuke_walk_2.png').convert_alpha()
-            sasuke_3 = pygame.image.load('Picture/sasuke/sasuke_walk_3.png').convert_alpha()
-            self.frames = [sasuke_1, sasuke_2, sasuke_3]
-        else:
-            kakashi_1 = pygame.image.load('Picture/kakashi/kakashi_run_1.png').convert_alpha()
-            kakashi_2 = pygame.image.load('Picture/kakashi/kakashi_run_2.png').convert_alpha()
-            kakashi_3 = pygame.image.load('Picture/kakashi/kakashi_run_3.png').convert_alpha()
-            self.frames = [kakashi_1, kakashi_2, kakashi_3]
+        self.speed = 0.7
 
-        if type == 'kakashi':
-            self.speed = 5
+        if type == 'naruto':
+            self.frames = naruto_frames
+
+        elif type == 'sasuke':
+            self.frames = sasuke_frame
+
         else:
+            self.frames = kakashi_frame
             self.speed = 2
 
         self.position_list_y = position_list_y
-
-        self.frames = [pygame.transform.scale(frame, (84, 40)) for frame in self.frames]
 
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
@@ -136,21 +150,11 @@ class Zombie(pygame.sprite.Sprite):
         self.rect.x -= self.speed
 
 
-pygame.init()  # starting code
-
-screen = pygame.display.set_mode((1000, 600))  # screen size
-pygame.display.set_caption('Plant vs Zombie')  # title name
-clock = pygame.time.Clock()
-game_active = True
-# bg_music = pygame.mixer.Sound('audio/Plants vs. Zombies (Main Theme).mp3')
-# bg_music.play(loops=-1)
-
-
 # Groups
 zombie_groups = pygame.sprite.Group()
 
+# Set up surface and rectangle
 welcome_fp = create_file_path('Picture/welcome.png')
-
 welcome_surface = pygame.image.load(welcome_fp).convert()
 welcome_surface = pygame.transform.scale(welcome_surface, (1000, 600))
 
@@ -167,13 +171,14 @@ background_fp = create_file_path('Picture/game_background_pokemon.png')
 background_surface = pygame.image.load(background_fp).convert()
 background_surface = pygame.transform.scale(background_surface, (1000, 600))
 
-# Zombie timer
+# set up Zombie timer
 zombie_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(zombie_timer, 1500)
 
 game_start = False
 
 while True:
+    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -182,18 +187,18 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN and white_rectangle.collidepoint(event.pos):
             game_start = True
 
+        if event.type == zombie_timer and game_start:
+            zombie_groups.add(Zombie(choice(['naruto', 'naruto', 'sasuke', 'sasuke', 'kakashi']),
+                                     position_list_y=[150, 230, 310, 395, 480]))
+
     if game_active:
         screen.blit(white_surface, white_rectangle)
         screen.blit(welcome_surface, (0, 0))
         screen.blit(username_surface, username_rectangle)
+
         if game_start:
-            time.sleep(1)
             screen.blit(background_surface, (0, 0))
 
-            for event in pygame.event.get():
-                if event.type == zombie_timer:
-                    zombie_groups.add(Zombie(choice(['naruto', 'naruto', 'sasuke', 'sasuke', 'kakashi']),
-                                             position_list_y=[150, 230, 310, 395, 480]))
             zombie_groups.draw(screen)
             zombie_groups.update()
 
