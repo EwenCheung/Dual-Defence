fa02562462fb4a5e9814959138d75e2429bf81d0
DAKHINESWARAN <145662309+DAKHINESWARAN@users.noreply.github.com>
2023-12-27 22:22:31 +0800

    update again

--- test.py
+++ test.py
@@ -87,7 +87,6 @@ while asking_log_in_method:
 
 # game
 import pygame
-import time
 from sys import exit
 from random import randint, choice
 
@@ -97,51 +96,12 @@ screen = pygame.display.set_mode((1000, 600))  # screen size
 pygame.display.set_caption('Pokemon vs Naruto')  # title name
 clock = pygame.time.Clock()
 game_active = True
-bg_music = pygame.mixer.Sound('audio/Plants vs. Zombies (Main Theme).mp3')
-bg_music.play(loops=-1)
 
-
-class Game:
-    def __init__(self):
-        # Set up a custom event for the timer
-        self.timer_event = pygame.USEREVENT + 1
-
-        # Set the timer to generate the custom event every 1000 milliseconds (1 second)
-        pygame.time.set_timer(self.timer_event, 1000)
-
-        # Limit for the number of timer events (5 minutes)
-        self.timer_limit_minutes = 15
-        self.timer_limit_seconds = self.timer_limit_minutes * 60
-        self.timer_limit_milliseconds = self.timer_limit_seconds * 1000
-
-        # Variables for tracking game state
-        self.timer_limit_reached = False
-        self.timer_count = 0
-
-        # Font for displaying text on the screen
-        self.font = pygame.font.Font(None, 36)
-
-    def handle_events(self):
-        for event in pygame.event.get():
-            # Handle the custom timer event
-            if event.type == self.timer_event and not self.timer_limit_reached:
-                self.timer_count += 1
-                print("Tik")
-
-                # Check if the limit is reached
-                if pygame.time.get_ticks() >= self.timer_limit_milliseconds:
-                    pygame.time.set_timer(self.timer_event, 0)  # Stop the timer
-                    self.timer_limit_reached = True
-                    print("Time's Up.")
-
-    def display_message(self, message):
-        text_surface = self.font.render(message, True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
-        self.screen.blit(text_surface, text_rect)
-
-    def run(self):
-        clock = pygame.time.Clock()
-
+font = pygame.font.Font(None, 36) 
+black = (0, 0, 0)  
+wood_color = (156, 102, 31)  
+countdown_duration = 1000  
+start_time = pygame.time.get_ticks()
 
 # load images
 naruto_frames = [pygame.image.load('Picture/naruto/naruto_walk_1.png').convert_alpha(),
@@ -235,22 +195,13 @@ num_ball_font = pygame.font.Font(None, 30)
 num_ball_surface = num_ball_font.render(str(num_ball), None, 'Black')
 num_ball_rectangle = num_ball_surface.get_rect(center=(65, 85))
 
+
 # set up Zombie timer
 zombie_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(zombie_timer, 1500)
 
 game_start = False
 active_pokemon = None
-
-# set a time limit for game
-game_timer = pygame.USEREVENT + 1
-timer_minutes = 1
-timer_seconds = timer_minutes * 60
-timer_milliseconds = timer_seconds * 1000
-pygame.time.set_timer(game_timer, timer_milliseconds)
-timer_count = 0
-game_over = False
-font = pygame.font.Font(None, 48)
 
 while True:
     # Event handling
@@ -262,12 +213,12 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN and white_rectangle.collidepoint(event.pos):
             game_start = True
 
-        # zombie
+        #zombie
         if event.type == zombie_timer and game_start:
             zombie_groups.add(Zombie(choice(['naruto', 'naruto', 'sasuke', 'sasuke', 'kakashi']),
                                      position_list_y=[180, 280, 370, 445, 540]))
 
-        # drag pokemon
+        #drag pokemon
         if event.type == pygame.MOUSEBUTTONDOWN:
             if machine_card_rectangle.collidepoint(event.pos):
                 active_pokemon = 'machine'
@@ -285,6 +236,7 @@ while True:
                 elif active_pokemon == 'squirtle':
                     num_ball -= 100
 
+
                 active_pokemon = None
 
         if active_pokemon == 'machine' and event.type == pygame.MOUSEMOTION:
@@ -299,52 +251,39 @@ while True:
             # Move the card by the mouse motion offset
             squirtle_card_rectangle.move_ip(event.rel)
 
+
+
     if game_active:
         screen.blit(white_surface, white_rectangle)
         screen.blit(welcome_surface, (0, 0))
         screen.blit(username_surface, username_rectangle)
 
         if game_start:
+            num_ball_surface = num_ball_font.render(str(num_ball), None, 'Black')
             screen.blit(background_surface, (0, 0))
             screen.blit(machine_card_surface, machine_card_rectangle)
             screen.blit(pikachu_card_surface, pikachu_card_rectangle)
             screen.blit(squirtle_card_surface, squirtle_card_rectangle)
             screen.blit(num_ball_surface, num_ball_rectangle)
-            num_ball_surface = num_ball_font.render(str(num_ball), None, 'Black')
 
             zombie_groups.draw(screen)
             zombie_groups.update()
 
-        if event.type == game_timer and not game_over:
-            timer_count += 1
-            print(int(timer_count))
-
-        if timer_count >= timer_seconds:
-            pygame.time.set_timer(game_timer, 0)
-            game_over = True
-            print("You've finished the course!.")
-
-
-    def display_message(self, message):
-        text_surface = self.font.render(message, True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
-        self.screen.blit(text_surface, text_rect)
-        return "You've Won"
-
-
-    def run(self):
-        clock = pygame.time.Clock()
-
-        while not self.game_over:
-            self.handle_events()
-
-
-    pygame.display.update()
-    clock.tick(60)
-
-    display_message()
+    current_time = pygame.time.get_ticks()
+    elapsed_time = current_time - start_time
+    remaining_time = max(0, countdown_duration - elapsed_time)
+    minutes = remaining_time // 60000
+    seconds = (remaining_time % 60000) // 1000
+    wood_box_rect = pygame.Rect(400, 0, 325, 60) # (x,y,width,height)
+    pygame.draw.rect(screen, wood_color, wood_box_rect)
+    text_timer = font.render(f"{minutes:02}:{seconds:02}", True, black)
+    text_timer_rect = text_timer.get_rect(center=wood_box_rect.center)
+    screen.blit(text_timer, text_timer_rect)
+ 
     pygame.display.flip()
-    pygame.time.delay(3000)
+    pygame.display.update()
+
+    clock.tick(60)
 
 # both
 # line (x,y)
