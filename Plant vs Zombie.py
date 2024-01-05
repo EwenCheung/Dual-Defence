e518341a57a011f0ec716f87e22818fec752e951
EwenCheung <142422759+EwenCheung@users.noreply.github.com>
2024-01-05 15:30:33 +0800

    doing grid for planting plants

--- Plant vs Zombie.py
+++ Plant vs Zombie.py
@@ -96,16 +96,29 @@ pygame.init()
 pygame.display.set_caption('Pokemon vs Naruto')  # title name
 pygame.display.set_mode((1000, 600))
 
+class Tools:
+    def __init__(self):
+
+        self.grid_coor = [[(325, 172), (325, 262), (325, 352), (325, 442), (325, 532)],
+                          [(410, 172), (410, 262), (410, 352), (410, 442), (410, 532)],
+                          [(495, 172), (495, 262), (495, 352), (495, 442), (495, 532)],
+                          [(586, 172), (586, 262), (586, 352), (586, 442), (586, 532)],
+                          [(670, 172), (670, 262), (670, 352), (670, 442), (670, 532)]]
+    def find_grid_coor(self, pos):
+        # check at which column (finding coordinate x)
+        for i, column in enumerate(self.grid_coor):
+            # cause our grid_coor is center so use + and - to get the max result
+            if self.grid_coor[i][0][0] - 42 <= pos[0] and self.grid_coor[i][0][0] + 42 >= pos[0]:
+                # check at which row (finding coordinate y), will output the coor for x and y
+                for coor in column:
+                    if coor[1] - 45 <= pos[1] and coor[1] + 45 >= pos[1]:
+                        return coor
 
 class Plant(pygame.sprite.Sprite):
     # center coordinate for each box
     # x = [325, 410, 495, 586, 670]
     # y = [172, 260, 355, 445, 532]
-    grid_coor = [(325, 172), (325, 260), (325, 355), (325, 445), (325, 532),
-                 (410, 172), (410, 260), (410, 355), (410, 445), (410, 532),
-                 (495, 172), (495, 260), (495, 355), (495, 445), (495, 532),
-                 (586, 172), (586, 260), (586, 355), (586, 445), (586, 532),
-                 (670, 172), (670, 260), (670, 355), (670, 445), (670, 532)]
+
 
     # plant
     MACHINE_FRAMES = [pygame.image.load('Picture/machine/machine_1.jpeg').convert_alpha(),
@@ -121,7 +134,7 @@ class Plant(pygame.sprite.Sprite):
                       pygame.image.load('Picture/pikachu/pikachu_3.png').convert_alpha(),
                       pygame.image.load('Picture/pikachu/pikachu_4.png').convert_alpha()]
 
-    def __init__(self, plant_type):
+    def __init__(self, plant_type, planting_coordinate):
         super().__init__()
 
         self.plant_type = plant_type
@@ -143,7 +156,7 @@ class Plant(pygame.sprite.Sprite):
 
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
-        self.rect = self.image.get_rect(center=(choice(self.grid_coor)))
+        self.rect = self.image.get_rect(center=(planting_coordinate))
 
     def update_animation_state(self):
         self.animation_index += 0.1
@@ -181,11 +194,11 @@ class Ninja(pygame.sprite.Sprite):
         self.speed = 1
 
         if ninja_type == 'naruto':
-            self.frames = [pygame.transform.scale(frame, (84, 40)) for frame in self.NARUTO_FRAMES]
+            self.frames = [pygame.transform.scale(frame, (84, 45)) for frame in self.NARUTO_FRAMES]
         elif ninja_type == 'sasuke':
-            self.frames = [pygame.transform.scale(frame, (84, 40)) for frame in self.SASUKE_FRAMES]
+            self.frames = [pygame.transform.scale(frame, (75, 55)) for frame in self.SASUKE_FRAMES]
         elif ninja_type == 'kakashi':
-            self.frames = [pygame.transform.scale(frame, (84, 40)) for frame in self.KAKASHI_FRAMES]
+            self.frames = [pygame.transform.scale(frame, (90, 60)) for frame in self.KAKASHI_FRAMES]
             self.speed = 2
         else:
             print('No ninja found')
@@ -221,6 +234,7 @@ class Game():
         self.before_press_start = True
         self.after_press_start = False
         self.chosen_pokemon = None
+        # self.coordinate = (0, 0)
 
         # Groups
         self.ninja_groups = pygame.sprite.Group()
@@ -321,8 +335,8 @@ class Game():
                         if not self.squirtle_card_rectangle.colliderect(self.squirtle_card_initial_position + (1, 1)):
                             self.squirtle_card_rectangle.topleft = self.squirtle_card_initial_position  # Snap back to initial position
 
-                    self.plant_groups.add(Plant(self.chosen_pokemon))
-
+                    coordinate = Tools().find_grid_coor(event.pos)
+                    self.plant_groups.add(Plant(self.chosen_pokemon, coordinate))
                     self.chosen_pokemon = None
 
     def game_start(self):
